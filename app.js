0a3e83a9de657e33057baeceeaea8a7680bf33f9
billywoo <bill@frogplum.com>
2018-09-06 15:52:31 -0700

    test css

--- app.js
+++ app.js
@@ -1,40 +1,51 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "environment" }, audio: false };
+var constraints = {
+  video: {
+    facingMode: "environment"
+  },
+  audio: false
+};
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger"),
-    cameraFlip = document.querySelector("#camera--flip")
+  cameraOutput = document.querySelector("#camera--output"),
+  cameraSensor = document.querySelector("#camera--sensor"),
+  cameraTrigger = document.querySelector("#camera--trigger"),
+  cameraFlip = document.querySelector("#camera--flip")
 
-  navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia || navigator.mediaDevices.webkitGetUserMedia || navigator.mediaDevices.mozGetUserMedia  || navigator.mediaDevices.oGetUserMedia || navigator.mediaDevices.msGetUserMedia
+navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia || navigator.mediaDevices.webkitGetUserMedia || navigator.mediaDevices.mozGetUserMedia || navigator.mediaDevices.oGetUserMedia || navigator.mediaDevices.msGetUserMedia
 // Access the device camera and stream to cameraView
 function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function (stream) {
+      track = stream.getTracks()[0];
+      cameraView.srcObject = stream;
     })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
+    .catch(function (error) {
+      console.error("Oops. Something is broken.", error);
     });
 }
-cameraFlip.onclick = function(){
-  if(constraints.video.facingMode === "environment"){
+cameraFlip.onclick = function () {
+  if (constraints.video.facingMode === "environment") {
     constraints.video.facingMode = "user"
-  }else{
+    $('#camera--flip').css({
+      'background-color': 'green'
+    });
+  } else {
     constraints.video.facingMode = "environment"
+    $('#camera--flip').css({
+      'background-color': 'yellow'
+    });
   }
   cameraStart()
 }
 // Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
+cameraTrigger.onclick = function () {
+  cameraSensor.width = cameraView.videoWidth;
+  cameraSensor.height = cameraView.videoHeight;
+  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+  cameraOutput.src = cameraSensor.toDataURL("image/webp");
+  cameraOutput.classList.add("taken");
 };
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
